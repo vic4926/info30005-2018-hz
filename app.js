eaec68d6dda553eb96c726f07c882db33ecbfca6
yttrium7 <yechen.vl@gmail.com>
2018-04-20 01:06:20 +1000

    oprn heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use(express.static('web v1.0'));
+app.use(express.static('web-v2.0'));
 
 app.listen(PORT, function(){
     console.log(`Express listening on port ${PORT}`);
